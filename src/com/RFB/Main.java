a26ea9eb223061b9d5ccf4e338d9b8d951d4cbe0
C-Lyngbjerg <56966598+C-Lyngbjerg@users.noreply.github.com>
2020-03-23 15:07:29 +0100

    updated Main
    Updated main with read & write methods

--- src/com/RFB/Main.java
+++ src/com/RFB/Main.java
@@ -6,26 +6,41 @@ import java.util.ArrayList;
 public class Main {
 
     public static void main(String[] args) throws FileNotFoundException {
-        readFromFile();
-        ArrayList<WorkSchedule> schedule = WorkSchedule.readFromFile();
-        System.out.println(schedule);
-        System.out.println("Hej");
-        System.out.println(System.getProperty("file.separator"));
+        ArrayList<Employee> employees = new ArrayList<>();
+        ArrayList<City> cities = new ArrayList<>();
+        ArrayList<Parent> parents = new ArrayList<>();
+        ArrayList<Child> children = new ArrayList<>();
+        ArrayList<WorkSchedule> schedules = new ArrayList<>();
+        ArrayList<Room> rooms = new ArrayList<>();
+        ArrayList<Household> households = new ArrayList<>();
+        readFromFile(employees, cities, parents, children, schedules, rooms, households);
+        writeToFile(employees, cities, parents, children, schedules, rooms, households);
+        System.out.println("HEJ");
     }
+    
 
-    public static void readFromFile() throws FileNotFoundException {
-        //ArrayList<Employee> employeeAL = new ArrayList<Employee>();
-        //ArrayList<City> cities = City.readCity();
-        //System.out.println(cities.get(0));
-        //ArrayList<Parent> parents = Parent.readParent();
+    public static void readFromFile(ArrayList<Employee> employees, ArrayList<City> cities, ArrayList<Parent> parents, ArrayList<Child> children, ArrayList<WorkSchedule> schedules, ArrayList<Room> rooms, ArrayList<Household> households) throws FileNotFoundException {
         /*
+        ArrayList<Employee> employees = Employee.readEmployee();
+        ArrayList<City> cities = City.readCity();
+        ArrayList<Parent> parents = Parent.readParent();
         ArrayList<Child> children = Child.readFromFile();
         ArrayList<WorkSchedule> schedules = WorkSchedule.readFromFile();
         ArrayList<Room> rooms = Room.readFromFile();
         ArrayList<Household> households = Household.readFromFile();
         ArrayList<Employee> employeeAL = Employee.readEmployee();
          */
-        //ArrayList<WorkSchedule> schedule = WorkSchedule.readFromFile();
-        //System.out.println(schedule.get(0));
+    }
+
+    public static void writeToFile(ArrayList<Employee> employees, ArrayList<City> cities, ArrayList<Parent> parents, ArrayList<Child> children, ArrayList<WorkSchedule> schedules, ArrayList<Room> rooms, ArrayList<Household> households) throws FileNotFoundException {
+        /*
+        Employee.writeEmployee(employees);
+        City.writeCity(cities);
+        Parent.writeParent(parents);
+        Child.writeToFile(children);
+        WorkSchedule.writeToFile(schedules);
+        Room.writeToFile(rooms);
+        Household.writeToFile(households);
+         */
     }
 }
