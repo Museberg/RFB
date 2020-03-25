ddabb4661a1cc15c21fdc36662384be98e84e143
Lbniese <lbniese@gmail.com>
2020-03-25 11:25:53 +0100

    Reformatted Employee class
    *Reformatted Employee class

--- src/com/RFB/Employee.java
+++ src/com/RFB/Employee.java
@@ -8,6 +8,9 @@ import java.util.Scanner;
 
 public class Employee {
 
+    // Used for file handling
+    private static String fs = System.getProperty("file.separator"); // File separator
+    private static String fileName = "src" + fs + "Database" + fs + "employee.txt";
     //Fields
     private int id;
     private String firstName;
@@ -15,7 +18,6 @@ public class Employee {
     private int phoneNumber;
     private int city_id;
     private String address;
-
     //Constructer
     public Employee(int id, String firstName, String lastName, int phoneNumber, int city_id, String address) {
         this.firstName = firstName;
@@ -25,9 +27,55 @@ public class Employee {
         this.address = address;
     }
 
-    // Used for file handling
-    private static String fs = System.getProperty("file.separator"); // File separator
-    private static String fileName = "src" + fs + "Database" + fs + "employee.txt";
+    //Add employee method
+    public static void addEmployee(ArrayList<Employee> employees) {
+        Scanner console = new Scanner(System.in);
+        System.out.println("Now adding a new employee");
+
+        System.out.print("Type in an ID for the employee: ");
+        int id = InputHelper.getIntFromUser();
+
+        System.out.print("Type in first name: ");
+        String firstName = console.next();
+
+        System.out.print("Type in last name: ");
+        String lastName = console.next();
+
+        System.out.print("Type in telephone number: ");
+        int phoneNumber = InputHelper.getPhoneNumberFromUser();
+
+        System.out.print("Type in city id: ");
+        int city_id = console.nextInt();
+        console.nextLine();
+
+        System.out.print("Type in address (street name and number): ");
+        String address = console.nextLine();
+
+        Employee tempEmployee = new Employee(id, firstName, lastName, phoneNumber, city_id, address);
+        employees.add(tempEmployee);
+    }
+
+    //Read from file method
+    public static ArrayList<Employee> readFromFile() throws FileNotFoundException {
+        Scanner input = new Scanner(new File(fileName));
+        String line;
+        ArrayList<Employee> employeeAL = new ArrayList<>();
+        while (input.hasNextLine()) {
+            line = input.nextLine();
+            String[] lineInput = line.split(",");
+            Employee tempEmployee = new Employee(Integer.parseInt(lineInput[0]), lineInput[1], lineInput[2], Integer.parseInt(lineInput[3]), Integer.parseInt(lineInput[4]), lineInput[5]);
+            employeeAL.add(tempEmployee);
+        }
+        return employeeAL;
+    }
+
+    //Write to a file method
+    public static void writeToFile(ArrayList<Employee> employees) throws FileNotFoundException {
+        PrintStream output = new PrintStream(new File(fileName));
+        for (int i = 0; i < employees.size(); i++) {
+            output.println(employees.get(i).toFile());
+        }
+    }
 
     //Getters & setters
     public String getFirstName() {
@@ -78,64 +126,14 @@ public class Employee {
         this.id = id;
     }
 
-    //Add employee method
-    public static void addEmployee(ArrayList<Employee> employees){
-        Scanner console = new Scanner(System.in);
-        System.out.println("Now adding a new employee");
-
-        System.out.print("Type in an ID for the employee: ");
-        int id = InputHelper.getIntFromUser();
-
-        System.out.print("Type in first name: ");
-        String firstName = console.next();
-
-        System.out.print("Type in last name: ");
-        String lastName = console.next();
-
-        System.out.print("Type in telephone number: ");
-        int phoneNumber = InputHelper.getPhoneNumberFromUser();
-
-        System.out.print("Type in city id: ");
-        int city_id = console.nextInt();
-        console.nextLine();
-
-        System.out.print("Type in address (street name and number): ");
-        String address = console.nextLine();
-
-        Employee tempEmployee = new Employee(id, firstName, lastName, phoneNumber, city_id, address);
-        employees.add(tempEmployee);
-    }
-
     //toString method
-    public String toString(){
-        return "Employee ID: " + id +"\nName: " + firstName + " " + lastName;
+    public String toString() {
+        return "Employee ID: " + id + "\nName: " + firstName + " " + lastName;
     }
 
     //toFile Method (used in method writeToFile when saving to a file)
-    public String toFile(){
+    public String toFile() {
         return getId() + "," + getFirstName() + "," + getLastName() + "," + getPhoneNumber() + "," + getCity_id() + "," + getAddress();
-    }
-
-    //Read from file method
-    public static ArrayList<Employee> readFromFile() throws FileNotFoundException {
-        Scanner input = new Scanner(new File(fileName));
-        String line;
-        ArrayList<Employee> employeeAL = new ArrayList<>();
-        while(input.hasNextLine()) {
-            line = input.nextLine();
-            String [] lineInput = line.split(",");
-            Employee tempEmployee = new Employee(Integer.parseInt(lineInput[0]), lineInput[1], lineInput[2], Integer.parseInt(lineInput[3]), Integer.parseInt(lineInput[4]), lineInput[5]);
-            employeeAL.add(tempEmployee);
-        }
-        return employeeAL;
-    }
-
-    //Write to a file method
-    public static void writeToFile(ArrayList<Employee> employees) throws FileNotFoundException {
-        PrintStream output = new PrintStream(new File(fileName));
-        for (int i = 0; i < employees.size(); i++) {
-            output.println(employees.get(i).toFile());
-        }
     }
 
 
