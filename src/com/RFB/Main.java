65ef6511c0c599c2b22bbcaf43c91173c1442fb8
Lbniese <lbniese@gmail.com>
2020-03-27 11:28:02 +0100

    Removed unused code in Main class
    *Removed unused code in Main class

--- src/com/RFB/Main.java
+++ src/com/RFB/Main.java
@@ -1,11 +1,8 @@
 package com.RFB;
 
-import java.io.FileNotFoundException;
-
 public class Main {
 
-
-    public static void main(String[] args) throws FileNotFoundException {
+    public static void main(String[] args) {
         Menu.startMenu();
     }
 
